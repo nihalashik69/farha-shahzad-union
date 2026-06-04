0e257b49c534c69b5bf6721db445910af0221ac1
nihalashik69 <mailnihal1995@gmail.com>
2026-06-04 21:47:48 +0300

    Remove generated vercel output

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-D8DgIORb.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Admin/farha-shahzad-union/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-CwEnjqfe.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-CwEnjqfe.js" } }] }, "/": { filePath: "C:/Users/Admin/farha-shahzad-union/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-CNOuq3kW.js"] } } });
-export {
-  tsrStartManifest
-};
